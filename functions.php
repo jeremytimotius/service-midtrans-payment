a268382bb3e9415cf2e92428773b660609132222
jeremytimotius <jeremytimotius08@gmail.com>
2020-05-29 16:35:46 +0700

    modified

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-    $connection = new mysqli("127.0.0.1", "root", "", "midtrans_dummy") or die('faileld: ' . $connection->error);
+    $connection = new mysqli("localhost", "root", "", "midtrans_dummy") or die('faileld: ' . $connection->error);
 
     if ($connection -> connect_errno) {
         echo "Failed to connect to MySQL: " . $connection -> connect_error;
