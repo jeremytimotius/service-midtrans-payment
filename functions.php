29c426778ae4d764c2a56d1b1a39f4e18a876cb6
jeremytimotius <jeremytimotius08@gmail.com>
2020-05-29 15:33:02 +0700

    modified

--- functions.php
+++ functions.php
@@ -2,6 +2,11 @@
     $connection = mysqli_connect("localhost", "root", "", "midtrans_dummy");
 
     function storeToDatabase($grossAmount){
+
+        echo "in";
+
+        var_dump($grossAmount);
+
         global $connection;
         
         mysqli_query($connection, "INSERT INTO `transactions`(`transactionId`, `gross_amount`) VALUES ('',$grossAmount)");
