0709b40386e1c191adda5be2c53a2ad14a7c01b6
jeremytimotius <jeremytimotius08@gmail.com>
2020-05-29 15:19:40 +0700

    modified

--- functions.php
+++ functions.php
@@ -2,9 +2,8 @@
     $connection = mysqli_connect("localhost", "root", "", "midtrans_dummy");
 
     function storeToDatabase($grossAmount){
-        
         global $connection;
-        mysqli_query($connection, "INSERT INTO transaction VALUES('', $grossAmount)");
+        mysqli_query($connection, "INSERT INTO transactions VALUES('', $grossAmount)");
         return mysqli_affected_rows($connection);
     }
 
